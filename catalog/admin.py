4aae45db4da7b8a3a8ef095016959dda5f80bd46
ANNA TOIMURZINA <annatoimurzina@MacBook-Pro-Anna.local>
2023-09-11 00:39:05 +0500

    модель version

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
-
+from catalog.models import Category, Product, Version
 
 '''admin.site.register(Category)
 admin.site.register(Product)'''
@@ -16,5 +15,11 @@ class ProductAdmin (admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')
     list_filter = ('name', 'price', 'category')
     search_fields = ('name', 'description')
+
+@admin.register(Version)
+class VersionAdmin (admin.ModelAdmin):
+    list_display = ('id', 'product', 'version_name', 'version_number', 'is_active',)
+    search_fields = ('product', 'version_name', 'version_number',)
+    list_filter = ('product', 'version_name', 'version_number', 'is_active',)
 
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,11 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView
 
-from catalog.forms import ProductForm
-from catalog.models import Product
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, Version
+
 
 class ProductListView(ListView):
     model = Product
@@ -26,7 +28,38 @@ class ProductCreateView(CreateView):
     success_url = reverse_lazy('catalog:about')
 
 class ProductUpdateView(UpdateView):
-    pass
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:about')
+
+    def get_context_data(self, **kwargs):
+        """Insert the form into the context dict."""
+
+        context_data = super().get_context_data(**kwargs)
+
+        version_form_set = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+
+        if self.request.method == 'POST':
+            context_data['formset'] = version_form_set(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = version_form_set(instance=self.object)
+
+        return context_data
+
+    def form_valid(self, form):
+        """Called if all forms are valid.
+         Creates a Version instance along with the main Product instance."""
+
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        self.object.owner = self.request.user
+        self.object.save()
+
+        if formset.is_valid():
+            formset.instance = self.object # Привязываем версию к продукту
+            formset.save()
+
+        return super().form_valid(form)
 
 
 
